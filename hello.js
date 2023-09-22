19766e4eb8548e4e88351426daade9ea715163af
binitworld <contactbinitbhushan@gmail.com>
2023-09-23 04:26:46 +0530

    23.2

--- hello.js
+++ hello.js
@@ -1,1 +1,6 @@
-console.log("Hello ! world guys");
+console.log("In this program, add 5,3,&50 ");
+function addThreeNumber(a,b,c){
+    return a+b+c;
+}
+let c = addThreeNumber(5,3,50)
+console.log(c)
