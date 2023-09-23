42b973f4827d63049de33f14e040df31a315dfda
binitworld <contactbinitbhushan@gmail.com>
2023-09-23 23:57:55 +0530

    1.v

---
+++ variable.js
@@ -0,0 +1,20 @@
+// VARIABLES:-->
+
+// Variables is a container that stores a value . This is very similar to the containers used to store  rice , water and acts (Treat this as an analogy !)
+// The value of a javascript variable can be changed during the execution  of program 
+
+//Rules for Variable:
+// -> letters digits , underscores & $ sign allowed.
+// -> Must begin with a$ , or a letter
+// -> java Script reversed  words cannot be used as a variable name .
+// -> harry & Harry are different variable (case sensitive)
+
+// _______________________________________________________________________________
+// here var and let is a literal(Type of constant value) where as a is identifiers = is assignment operator and 67 is a declaring variable: 
+let a =67 //a contains 67
+// var a = 67  tis is also applicable
+console.log(a)
+a = "Binit"   //Dynamic programming 
+console.log(a)
+
+
