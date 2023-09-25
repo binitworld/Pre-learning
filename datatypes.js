829503ea3e1b61658b2ebfbd2ae9b4e25ec371cb
binitworld <contactbinitbhushan@gmail.com>
2023-09-25 20:46:53 +0530

    25.9.23/-datatypes

---
+++ datatypes.js
@@ -0,0 +1,36 @@
+console.log("In This code we have to see the PERMEITIVE Data types and Objects ! in Java scripts. ")
+// nn bb ss u
+let a = null ;   // Null value
+let b = 543;    // Number 
+let c = true; //can also be false (Type of boolean )
+let d = BigInt("5889") ;//Types of BigInt
+let e = "King is busy in coding!" ;// It is a string
+let f =Symbol("I'm a symbol"); //Type of Symbol
+let g = undefined; //Undefined senario
+console.log(a,b,c,d,e,f,g)
+console.log(typeof a )
+console.log(typeof b )
+console.log(typeof d )
+console.log(typeof c )
+console.log(typeof e )
+console.log(typeof f )
+console.log(typeof g )
+console.log("These are all Premitive data type" )
+
+//Objects is type of non -data primitivetype
+console.log("OBJECTS!!-- Non -PRIMITIVE DATA TYPES")
+const item = {
+    "name" : "TajMahal",
+    "mridul": false,
+    "Harsh" : true,
+    "price": 3567,
+
+
+}
+console.log(item["mridul"])
+console.log(item["sjnjht"])
+console.log(item["name"])
+console.log(item["price"])
+console.log(item["Harsh"])
+
+// We can make Key value pairs in js and look ups it!! mapping types
