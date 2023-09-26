ca4fae5fd8b528d0ad5d4d64acd3efd08b49887c
binitworld <contactbinitbhushan@gmail.com>
2023-09-26 13:19:50 +0530

    26.09 ps.1-3

---
+++ practiice-set1-3.js
@@ -0,0 +1,7 @@
+//Create a constant object  in js .Can you change it to hold a number .later?
+const a={
+    name : 34,
+    section: 3,
+    isdoc :false
+}
+a = 56  //{In this code we can't add a number to hold as it through an error ""}
