d7fc61082914c7664e9b170c48708a6d2d6d487e
binitworld <contactbinitbhushan@gmail.com>
2023-09-26 14:32:09 +0530

    26.09 - logical operator ,js

---
+++ logical=operator.js
@@ -0,0 +1,18 @@
+console.log("Logical operator in js")
+
+
+let x= 7;
+let y= 9;
+
+// && - Logical and 
+console.log(x<y && x==7)
+
+// || - Logical or 
+console.log(x>y || x!=7)
+console.log(x<y || x!=7)
+
+// !  - Logical  not ==false to true & true to false
+console.log(!false)
+console.log(!true)
+
+
