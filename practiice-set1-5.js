4ba72b7717ab6f89d8dfd2753aa01ba79b8e1bcb
binitworld <contactbinitbhushan@gmail.com>
2023-09-26 13:34:26 +0530

    26-09-ps.1-5

---
+++ practiice-set1-5.js
@@ -0,0 +1,10 @@
+// Write a js program to create a word meaning dictionary of 5 words 
+const dictionary ={
+    Abnegation :"Renouncing a belief or doctrine.",
+    Aggrandize:'enhance power, wealth or status.',
+    Alacrity :"eagerness.",
+    Anachronistic:"misplaced chronologically.",
+    Archetypal :"quintessential of a certain kind.",
+    Ascetic : "one who practices self-denial as part of spiritual discipline."
+}
+console.log(dictionary)
