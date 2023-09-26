dde0e5341f849cee8ac9980da5313ec851421ff8
binitworld <contactbinitbhushan@gmail.com>
2023-09-26 13:26:12 +0530

    26-09-ps.1-4

---
+++ practiice-set1-4.js
@@ -0,0 +1,11 @@
+// Try to add  a new key to the const object in problem  where you able to do it?
+const a={
+    name : 34,
+    section: 3,
+    isdoc :false
+}
+a['Friend'] = "Bot"
+a['marks'] = 78
+
+console.log(a) 
+// Yes for ths question  we can add  a new key to the const object in problem and we are able to it
