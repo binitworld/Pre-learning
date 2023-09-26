78b71e2a554e0a11a29d5f622102a83d0d738fdf
binitworld <contactbinitbhushan@gmail.com>
2023-09-26 14:21:10 +0530

    26.09-9 comparison operator in js

---
+++ compsrision-operator.js
@@ -0,0 +1,30 @@
+console.log("Comparison Operator in js ")
+let a= 5;
+let b = 7;
+
+//==: equal to
+console.log('a==b is ', a == b)
+
+// != :not equal to 
+console .log ('a!=b is ', a!=b)
+
+// === : equal value and type 
+console.log('a===b is', a===b)
+
+// ! == : not equal value and type 
+console.log('a!==b is' ,a!==b)
+
+// > : greater than
+console.log ("a>b is" ,a>b)
+
+// < : smaller than
+console.log("a<b is ", a<b)  
+
+// > =: greater than or equal to 
+console.log("a>=b is", a>=b)
+
+// < = : smaller than or equal to 
+console.log("a<=b is", a<=b )
+
+// ? = ternary operator
+// console.log('a?b is', a?b ) 
