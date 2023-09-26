c96e87d34fc05ba6097ddd9a42ff27ad8704b75a
binitworld <contactbinitbhushan@gmail.com>
2023-09-26 11:28:09 +0530

    26.09.ps2

---
+++ practiice-set1-2.js
@@ -0,0 +1,7 @@
+// 2. Use type operator to find the data type of the string in last question
+var a = " Binit ";
+console.log(a)
+console.log(typeof a)
+var a = 45
+console.log(a)
+console.log(typeof a)
