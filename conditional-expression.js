8ad315da2817fd67986f3bf439212b5f0ac3c50a
binitworld <contactbinitbhushan@gmail.com>
2023-09-26 14:05:01 +0530

    26-09 .08.conditional expression

---
+++ conditional-expression.js
@@ -0,0 +1,72 @@
+console.log("operator in js")
+
+let a =56;
+let b =78;
+console.log( " a+b = " ,a+b)
+console.log( " a-b = " ,a-b)
+console.log( " a*b = " ,a*b)
+console.log( " a/b = " ,a/b) //it shows the exact value of remainder
+console.log( " a%b = " ,a%b)
+console.log( " a**b = " ,a**b)
+// console.log( " a++ = " ,a++) // increment model
+console.log( " a = " ,a)
+// console.log( " ++a = " ,++a)
+console.log( " a-- = " ,a--)
+console.log( " a = " ,a)
+
+// Assignment  Operator
+let assignment = 8;
+console.log(assignment)
+
+assignment += 5 ;
+console.log(assignment)
+
+assignment -= 5 ;
+console.log(assignment)
+
+assignment *= 5;
+console.log(assignment)
+
+assignment /= 2;
+console.log(assignment)
+// assignment == 4;
+// console.log(true)
+// assignment != 4;
+// console.log(false)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
