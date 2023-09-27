ad828e23a680b6ac3f5079a48929124e6b9d0438
binitworld <contactbinitbhushan@gmail.com>
2023-09-27 11:01:28 +0530

    27-09-ps2.2

---
+++ practice-set2-2.js
@@ -0,0 +1,22 @@
+
+//Demonstrate the use of switch case  statement in js 
+let readline = require("What is the price of box?");
+readline = parseInt(readline);
+switch (readline) {
+    case 78:
+        console.log("price is rs.10")
+    case 37:
+        console.log("price is rs.20")
+    case 12:
+        console.log("price is rs.20")
+    case 14:
+        console.log("price is rs.30")
+    case 15:
+        console.log("price is rs.40")
+    case 18: 
+        console.log("price is rs.50")
+
+}
+
+
+
