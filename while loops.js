91c8826b1e21bb5e3befca48a03da981ed794206
binitworld <contactbinitbhushan@gmail.com>
2023-09-30 03:34:01 +0530

    30.09.-01

---
+++ while loops.js
@@ -0,0 +1,11 @@
+console.log("Loops in java script")
+console.log("Types of loops in java script is 5 types: ")
+console.log('For loop:- loop a block of code no. of trees.')
+console.log('for in loop:- loops through the keys of an object')
+console.log('for of loop:- loops through the values of an object')
+console.log('while loop := loops a block based on a specific condition ')
+console.log('do while loop := while loops variant which runs at least once ')
+
+
+// while loop := loops a block based on a specific condition 
+
