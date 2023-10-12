4af69298ebe0dba4daf823cc1b288b3ec68fe0b5
Binit Bhushan <contactbinitbhushan@gmail.com>
2023-10-13 00:12:28 +0530

    Create fireworks.java

---
+++ src/fireworks.java
@@ -0,0 +1,12 @@
+// here is a tem location of learning Thing are under way to go 
+
+public class fireworks {
+    public static void main(System[] args){
+    int a=2;
+    int b=-2;
+    
+    int sum = a+b;
+    
+    System.out.println(sum);
+    
+    }
